90b94c5cfa629bdaf3ca9b59a7f3cd11c0f4a117
Alvin Liang <Alvin@Alvins-MBP.home>
2014-12-22 21:24:18 -0500

    started server file and fixed up css

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var fs = require("fs");
 var http = require("http");
 var server = http.createServer(function(request, response){
   var path = request.url;
+  var title = path.split("/");
   response.end()
 });
 server.listen(2000);
