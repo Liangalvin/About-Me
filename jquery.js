b7a67504607600d60000d1a5cd690caba46c432d
Alvin Liang <liangalvin@yahoo.com>
2014-12-26 11:43:10 -0500

    Added functionality to the buttons with Jquery and updated CSS

--- jquery.js
+++ jquery.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
   console.log("ready");
-  $("#nextButton").click(function(){
+  $("#nextButton").click(function(){ //on next button clicked
     console.log("clicked");
-    $("#slide1").slideUp("slow", function(){
-      $("#slide2").slideDown("slow", function(){
+    $("#slide1").slideUp("slow", function(){ //slide1 not visible
+      $("#slide2").slideDown("slow", function(){ //slide2 visible
         $("#slide2").css("display","inline");
-        console.log("done");
+        console.log("slide2 done");
+      });
+    });
+  });
+  $("#backButton").click(function(){  //on back button clicked
+    console.log("back");
+    $("#slide2").slideUp("slow", function(){ //slide2 not visible
+      $("#slide1").slideDown("slow", function(){ //slide 1 visible
+        $("#slide1").css("display", "inline");
+        console.log("slide1 done");
       });
     });
   });
